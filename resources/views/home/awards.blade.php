f03a9bc0aef2af1c0d56950f46b582b038bb3208
AdiPrabowoKusumo09 <adiprabowokusumo9@gmail.com>
2024-06-11 13:14:17 +0700

    New CV

--- resources/views/home/awards.blade.php
+++ resources/views/home/awards.blade.php
@@ -2,18 +2,26 @@
     <div class="container">
 
         <div class="section-title">
-            <h2>Sertifikasi</h2>
-            @foreach ($certifications as $certification)
-            <div class="resume-section-content">
-                <ul class="fa-ul mb-0">
-                    <li style="color: grey">
-                        <span class="fa-li"> <i class="fas fa-trophy text-warning"></i><strong>{{$certification->nama_sertifikasi}}</strong></span>
-                    </li>
-                    <i style="color: green">Diselenggarakan oleh : {{$certification->penyelenggara}} <br>
-                    Pada Tahun : {{$certification->tahun}} </i>
-                </ul>
-            </div>
-            @endforeach
+            <h2>Awards</h2>
+            <p>Check My Awards</p>
         </div>
+
+        <div class="row">
+
+                <h3 class="awards-title">Sertifikasi</h3>
+                @foreach ($certifications as $c )
+
+
+                <div class="awards-item">
+                    <h4>Nama Sertifikasi : {{ $c->nama_sertifikasi }}</h4>
+                    <h5> Diselenggarakan oleh : {{ $c->penyelenggara }}</h5>
+                    <p>Pada Tahun : {{ $c->tahun }}</p>
+                    {{-- <p>{{ $education->deskripsi }}</p> --}}
+                </div>
+                @endforeach
+            </div>
+
+        </div>
+
     </div>
 </section>
